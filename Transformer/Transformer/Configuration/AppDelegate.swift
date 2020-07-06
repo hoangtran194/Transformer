51f4bbc45164971f96e6fc111a86a4d4c9301490
Hoang Tran <hoangtran194@gmail.com>
2020-07-06 02:55:44 -0700

    update UI

--- Transformer/Transformer/Configuration/AppDelegate.swift
+++ Transformer/Transformer/Configuration/AppDelegate.swift
@@ -36,7 +36,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } else {
             self.window?.makeKeyAndVisible()
         }
-        UITabBar.appearance().tintColor = UIColor.red        
+//        UITabBar.appearance().tintColor = UIColor.red
+        
+//        let selectedColor   = UIColor(red: 246.0/255.0, green: 155.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+//        let unselectedColor = UIColor(red: 16.0/255.0, green: 224.0/255.0, blue: 223.0/255.0, alpha: 1.0)
+//        
+//        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: unselectedColor], for: .normal)
+//        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: selectedColor], for: .selected)
 
 
         return true
